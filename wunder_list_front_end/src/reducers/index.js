c53eda28aa366ede75dd9f86bd656310a53420ad
oashtari <oashtari@gmail.com>
2020-02-27 10:27:11 -0800

    set up reducers structure for all the actions

--- wunder_list_front_end/src/reducers/index.js
+++ wunder_list_front_end/src/reducers/index.js
@@ -1,4 +1,9 @@
-import { } from '../actions';
+import {
+    GET_LIST, LOAD_LIST, LIST_ERROR,
+    ADD_TODO, TODO_ADDED, ADD_ERROR,
+    EDIT_TODO, TODO_EDITED, EDIT_ERROR,
+    DELETE_TODO, TODO_DELETED, DELETE_ERROR
+} from '../actions';
 
 const initialState = {
     isLoading: false,
@@ -15,6 +20,82 @@ const initialState = {
 
 export const todoReducer = (state = initialState, action) => {
     switch (action.type) {
+
+        case GET_LIST:
+            return {
+                ...state,
+                isLoading: true,
+
+            }
+        case LOAD_LIST:
+            return {
+                ...state,
+                isLoading: false,
+
+            }
+        case LIST_ERROR:
+            return {
+                ...state,
+                isLoading: false,
+
+            }
+
+        case ADD_TODO:
+            return {
+                ...state,
+                isLoading: true,
+
+            }
+        case TODO_ADDED:
+            return {
+                ...state,
+                isLoading: false,
+
+            }
+        case ADD_ERROR:
+            return {
+                ...state,
+                isLoading: false,
+
+            }
+
+        case EDIT_TODO:
+            return {
+                ...state,
+                isLoading: true,
+
+            }
+        case TODO_EDITED:
+            return {
+                ...state,
+                isLoading: false,
+
+            }
+        case EDIT_ERROR:
+            return {
+                ...state,
+                isLoading: false,
+
+            }
+
+        case DELETE_TODO:
+            return {
+                ...state,
+                isLoading: true,
+
+            }
+        case TODO_DELETED:
+            return {
+                ...state,
+                isLoading: false,
+
+            }
+        case DELETE_ERROR:
+            return {
+                ...state,
+                isLoading: false,
+
+            }
 
         default:
             return state;
