b4cda6ca7e210bcbf38daa69e72ef236fc08d500
oashtari <oashtari@gmail.com>
2020-02-27 18:48:45 -0800

    data is rendering in the card item

--- wunder_list_front_end/src/components/CardItem.js
+++ wunder_list_front_end/src/components/CardItem.js
@@ -2,23 +2,26 @@ import React, { useState } from 'react';
 import { Card, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 // import { Route, Link } from "react-router-dom";
 
+
+
 export default function CardItem(props) {
   // [isDone, setIsDone] = useState(false);
 
   // Not sure if we will handle the state via useState or redux
-  console.log('testing props in card item', props.props.list);
+  console.log('testing props in card item', props.props.key);
+
   return (
     <div className='cardItem'>
       <Card>
         <CardBody>
           {/* <CardText className='editlink' onEdit={editPop}>Edit</CardText> */}
           {/* Insert route, link to an edit page/popover */}
-          <CardTitle>{props.props.list.title}</CardTitle>
-          <CardSubtitle>props.data.status</CardSubtitle>
-          <CardText>props.data.description</CardText>
+          <CardTitle>{props.props.title}</CardTitle>
+          <CardSubtitle>{props.props.status}</CardSubtitle>
+          <CardText>{props.props.created_date}</CardText>
           {/* <Button className='donebtn' onDone={doneStatus}>Done</Button> */}
           {/* Done button will change status of the ticket. This element is NOT available for already 'Done' todo cards */}
-          <CardText>props.data.duedate</CardText>
+          <CardText>{props.props.due_date}</CardText>
         </CardBody>
       </Card>
     </div>
